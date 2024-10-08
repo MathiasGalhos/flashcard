f23faab2b9b151ef9dcbdcbd214f89351320fc90
MathiasGalhos <159156650+MathiasGalhos@users.noreply.github.com>
2024-10-08 12:19:28 -0300

    08/10

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programção'
+)
+
+criaCartao(
+    'Felipe',
+    'É um homossexual?',
+    'É'
+)
+
+criaCartao(
+    'Daniel',
+    'O maluco tem',
+    '6 mil horas de CS'
+)
+
+criaCartao(
+    'Akirame no warui tayou',
+    'Anta ra nanka ja tabanatte mo kanawan',
+    'Kuwabara (3x)'
+)
